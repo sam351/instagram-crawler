93cc69a09cdd513dfa851a8a14a8eae2069b7b63
sam <ksmo9191@hanmail.net>
2020-02-11 00:49:52 +0900

    Add picture_crawler.py

---
+++ picture_crawler.py
@@ -0,0 +1,9 @@
+# < Example : python picture_crawler.py -o output_(유저명) --likes 200 >
+
+# 필요 라이브러리 import
+
+if __name__ == '__main__':
+    # ./output/ 디렉토리에서 바이너리 파일 오픈
+    # urlRetrieve
+    # ./output/유저명/유저명_index_likes 으로 저장
+    pass

82a9219c1b1430eae832c640a68eaa99d8e19426
sam <ksmo9191@hanmail.net>
2020-02-11 16:17:22 +0900

    Modify picture_crawler.py (to change into command-line interfaces)

--- picture_crawler.py
+++ picture_crawler.py
@@ -1,44 +1,73 @@
+import argparse
 import json
-from urllib.request import urlretrieve
 import os
-user_name = 'fashionsnapcom'
-
-# 1. Open the output binary file of crawler.py
-with open('./output/output_'+user_name, encoding='utf-8') as fp:
-    data = fp.read()
-    pass
-data_list = json.loads(data)
-# data_list  # check the result
-# len(data_list)  # check the result
-# len(data_list[101]['img_urls'])  # check the result
-
-# 2. Make a new directory to save pictures
-dir_path = './output/pictures_'+user_name
-if not os.path.exists(dir_path):
-        os.mkdir(dir_path)
-        print("Directory " , dir_path ,  " Created ")
-else:    
-        print("Directory " , dir_path ,  " already exists")
-
-# 3. retrieve & save pictures from instagram
-for idx, item in enumerate(data_list):
-    idx += 1
-    if len(item['img_urls']) != 0:
-        tmp_url = item['img_urls'][0]
-        urlretrieve(tmp_url, dir_path+f'/{user_name}_{idx}.png')
-
-    if idx % 100 == 0:
-        print(f'>>> picture {idx} has been successfully downloaded.')
-    pass
-print(f'download successfully completed')
+import sys
+from urllib.request import urlretrieve
 
 
-# ---------- ignore : To be developed (for running in prompt) -----------
-# < Example : python picture_crawler.py -o output_(유저명) --likes 200 >
+def usage():
+    return """
+        python picture_crawler.py -u cal_foodie -l 200
+        The default likes number for filtering (-l --likes option) is 100.
+    """
+
+
+def retrieve_pictures(username, likes):
+    # Open the output binary file of crawler.py
+    with open('./output/output_'+username, encoding='utf-8') as fp:
+        data = fp.read()
+        pass
+    data_list = json.loads(data)
+    print("json file successfully loaded")
+
+    # Make a new directory to save pictures
+    dir_path = './output/pictures_'+username
+    if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
+            print("Directory " , dir_path ,  " Created")
+    else:    
+            print("Directory " , dir_path ,  " already exists")
+
+    # retrieve & save pictures from instagram
+    # (only picutres with likes more than likes numbers)
+    for idx, item in enumerate(data_list):
+        idx += 1
+
+        tmp_likes = item['likes']
+        if tmp_likes < likes:
+            continue
+
+        if len(item['img_urls']) != 0:
+            tmp_url = item['img_urls'][0]
+
+            title_idx = '00'+str(idx)
+            title_idx = title_idx[-3:]
+            urlretrieve(tmp_url, dir_path+f'/{username}_{title_idx}_{tmp_likes}Likes.png')
+
+        if idx % 100 == 0:
+            print(f'>>> picture {idx} has been successfully downloaded.')
+        pass
+    print(f'download successfully completed')
+
+
+def arg_required(args, fields=[]):
+    for field in fields:
+        if not getattr(args, field):
+            parser.print_help()
+            sys.exit()
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser( description='Retrieve pictures using urls in binary', usage=usage() )
+    parser.add_argument('-u', '--username', help="instagram's username")
+    parser.add_argument('-l', '--likes', type=int, default=100,
+                        help="number of likes - picutres with likes less than that number would be ignored")
+
+    args = parser.parse_args()
+
+    arg_required('username')
+    retrieve_pictures(args.username, args.likes)
     # ./output/ 디렉토리에서 바이너리 파일 오픈
     # urlRetrieve
     # ./output/유저명/유저명_index_likes 으로 저장
     pass
-
-
